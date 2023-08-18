f55054983c1e5047c49b520f062f783b8c532e78
arkemongg <ark.emongg@gmail.com>
2023-08-19 03:24:33 +0600

    ok

--- src/index.js
+++ src/index.js
@@ -1,33 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.js';
-function Car(props) {
-    return <li>I am a { props.brand }</li>;
-  }
-  
-  function Garage() {
-    const cars = [
-        { id: 1, brand: 'Ford' },
-        { id: 2, brand: 'BMW' },
-        { id: 3, brand: 'Audi' }
-      ];
-    
-      const carComponents = [];
-    
-      for (let i = 0; i < cars.length; i++) {
-        const car = cars[i];
-        carComponents.push(<Car key={car.id} brand={car.brand} />);
-      }
-    return (
-      <>
-        <h1>Who lives in my garage?</h1>
-        <ul>
-            {carComponents}
-        </ul>
-      </>
-    );
-  }
-  
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Garage />);
+import { useState } from "react";
+import ReactDOM from "react-dom/client";
+import Todos from "./todos";
 
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState(["todo 1", "todo 2"]);
+
+  const increment = () => {
+    setTodos([...todos, `todo ${todos.length + 1}`])
+    setCount((c) => c + 1);
+  };
+
+  return (
+    <>
+      <Todos todos={todos} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
