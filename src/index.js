86dbe47e6ecf62df205e44d8a25240c6cf5a3dbb
arkemongg <ark.emongg@gmail.com>
2023-08-20 05:07:34 +0600

    testing hooks

--- src/index.js
+++ src/index.js
@@ -1,23 +1,45 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import Todos from "./todos";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [todos, setTodos] = useState(["todo 1", "todo 2"]);
+  const [api_url, setApi_url] = useState('http://127.0.0.1:8000/api/products/');
+  const [data,setData] = useState([{title:50}])
 
-  const increment = () => {
-    setTodos([...todos, `todo ${todos.length + 1}`])
-    setCount((c) => c + 1);
-  };
-
+  useEffect(()=>{
+    const timeoutId = setTimeout(() => {
+      loaddata()
+    }, 1000);
+    return () => {
+      clearTimeout(timeoutId); // Clean up the timeout when the component unmounts or the effect re-runs
+    };
+  },[api_url])
+  function loaddata(){
+    fetch(api_url)
+    .then(response => response.json())
+    .then(data=>{
+      setData(prevData=>[...prevData,...data.results])
+    })
+  }
+  const nextdata = ()=>{
+    setApi_url('https://jsonplaceholder.typicode.com/todos/2')
+  }
   return (
     <>
-      <Todos todos={todos} />
       <hr />
       <div>
-        Count: {count}
-        <button onClick={increment}>+</button>
+      <ul>
+          {data.map((item, index) => (
+            
+            <li key={index}>
+              {item.title}{console.log(item)}
+            </li>
+          ))}
+        </ul>
+
+        <br />
+        <button onClick={loaddata}>+</button>
+        <button onClick={nextdata}>next</button>
       </div>
     </>
   );
